20e695e35d813ac53eab40320f3c95e84ea2ea1c
weselwit13 <giulia.marcora32@gmail.com>
2023-11-14 17:13:13 +0100

    adding files

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const slider = document.getElementById('year');
+const currentValue = document.getElementById('current-value');
+
+const slider2 = document.getElementById('chapter');
+const currentValue2 = document.getElementById('current-valueb');
+
+slider.addEventListener('input', function() {
+    const min = parseInt(this.min);
+    const max = parseInt(this.max);
+    const percent = ((this.value - min) / (max - min)) * 100;
+
+    const thumbWidth = parseFloat(getComputedStyle(this).getPropertyValue('--thumb-width')) || 15;
+    const thumbPosition = (percent / 100) * (this.offsetWidth - thumbWidth);
+
+    currentValue.textContent = this.value;
+    currentValue.style.left = `calc(${thumbPosition}px - ${thumbWidth / 2}px)`;
+
+});
+
+slider2.addEventListener('input', function() {
+    const min = parseInt(this.min);
+    const max = parseInt(this.max);
+    const percent = ((this.value - min) / (max - min)) * 100;
+
+    const thumbWidth = parseFloat(getComputedStyle(this).getPropertyValue('--thumb-width')) || 15;
+    const thumbPosition = (percent / 100) * (this.offsetWidth - thumbWidth);
+
+    currentValue2.textContent = this.value;
+    currentValue2.style.left = `calc(${thumbPosition}px - ${thumbWidth / 2}px )`;
+    currentValue2.style.transform = 'translateX(1650%)'
+
+});
